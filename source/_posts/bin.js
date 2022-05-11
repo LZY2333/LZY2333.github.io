b125d4a4d876eb09bd930e9ab094626c524cedde
LZY2333 <1114396573@qq.com>
2022-05-11 17:20:26 +0800

    mianshi

--- source/_posts/bin.js
+++
@@ -1,9 +0,0 @@
-const simpleGit = require('simple-git');
-const git = simpleGit();
-git
-.init()
-.add('.')
-.commit(global.process.argv[2])
-.push([],()=> {
-    console.log("Push to master success");
-}) 
